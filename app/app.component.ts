f00bf4beac9f197e043ffa17374345ec9cc86883
Alex Pop <palexandru@gmail.com>
2016-12-13 09:31:00 +0000

    formatted html

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,25 +3,24 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <div class="container text-center">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="page-header">
-            <h1>{{title}}</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="page-header">
+              <h1>{{title}}</h1>
+            </div>
+            <p class="lead">{{description}}</p>
           </div>
-          <p class="lead">{{description}}</p>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-6">
-          <p>A child component could go here</p>
-        </div>
-        <div class="col-md-6">
-          <p>Another child component could go here</p>
-        </div>
-      </div>          
-    </div>    
-    `
+        <div class="row">
+          <div class="col-md-6">
+            <p>A child component could go here</p>
+          </div>
+          <div class="col-md-6">
+            <p>Another child component could go here</p>
+          </div>
+        </div>          
+      </div>`
 })
 export class AppComponent { 
   title: string;
